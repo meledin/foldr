c5a0a7038235c80d5124fcd22742e723205a9c51
Vladimir Katardjiev <gh@d2dx.com>
2013-09-28 13:32:07 -0700

    Initial Commit

---
+++ Foldr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Foldr
+//
+//  Created by Vladimir Katardjiev on 2013-09-28.
+//  Copyright (c) 2013 d2dx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Foldr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Foldr
+//
+//  Created by Vladimir Katardjiev on 2013-09-28.
+//  Copyright (c) 2013 d2dx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
