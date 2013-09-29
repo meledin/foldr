491d72fbae75c19c725a861069d4b6670fd8fb88
Vladimir Katardjiev <vladi@vlads-mac-pro.katardjiev.net>
2013-09-29 00:49:52 -0700

    More commits.

---
+++ Foldr/SerialDict.h
@@ -0,0 +1,13 @@
+//
+//  SerialDict.h
+//  Foldr
+//
+//  Created by Vladimir Katardjiev on 9/28/13.
+//  Copyright (c) 2013 d2dx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SerialDict : NSObject
+
+@end
---
+++ Foldr/SerialDict.m
@@ -0,0 +1,13 @@
+//
+//  SerialDict.m
+//  Foldr
+//
+//  Created by Vladimir Katardjiev on 9/28/13.
+//  Copyright (c) 2013 d2dx. All rights reserved.
+//
+
+#import "SerialDict.h"
+
+@implementation SerialDict
+
+@end
